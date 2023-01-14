60318ef98984c2908c162820ce60e73cd19f9ff6
“davit <davitdvalashvili@gmail.com>
2023-01-14 19:48:35 +0400

    edit index.js file content

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ card.addEventListener("click", active)
 
 function active(event) {
     if (event.target.matches(".question")) {
+        if(event.target.style.fontWeight != 400) {
+           event.target.parentElement.nextElementSibling.style.display = "none";
+           event.target.style.fontWeight = 400;
+           event.target.nextElementSibling.style.transform = "unset";
+           return
+        }
+        for(let i = 0; i < document.getElementsByClassName("question").length; i++) {
+            document.getElementsByClassName("question")[i].style.fontWeight = 400;
+            document.getElementsByClassName("answer")[i].style.display = "none";
+            document.getElementsByClassName("arrow")[i].style.transform = "unset"
+        }
         if (event.target.style.fontWeight == 400) {
             event.target.parentElement.nextElementSibling.style.display = "block";
             event.target.style.fontWeight = 700;
             event.target.nextElementSibling.style.transform = "rotate(180deg)";
-            return;
-        }
-        event.target.parentElement.nextElementSibling.style.display = "none";
-        event.target.style.fontWeight = 400;
-        event.target.nextElementSibling.style.transform = "unset"
+            return
+        }  
     }
 }
-// for(let i= 0; i < card.getElementsByClassName(".question").length ;i++) {
-//     card.getElementsByClassName("question")[i].style.fontWeight = 400;
-//     card.getElementsByClassName("answer")[i].style.display = "none";
-//     card.getElementsByClassName("arrow")[i].style.transform = "unset"
-// }
 
 
